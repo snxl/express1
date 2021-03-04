b752e353642120bf094e300629e56d85074e83ed
Nicolas <nicolas.soares.gomes@gmail.com>
2021-03-03 21:47:05 -0300

    finalizando estudo do query e params

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
-
 const app = express()
 
-app.get("/", (req, res) => {
-    return res.send("hello world")
+let array = [
+    "nicolas",
+    "ivens"
+]
+
+app.get("/hello/:usuario", (req, res) => {
+    let {usuario} = req.params
+    return res.send("hello world" + " " + usuario)
+})
+
+app.get('/usuarios', (req, res) => {
+    const {nome} =  req.query
+    let listaRetorno = array.filter(i => i.includes(nome || ""))
+    res.json(listaRetorno)
 })
 
 app.listen(80, () => {
